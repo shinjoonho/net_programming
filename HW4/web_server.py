2b605876cd74a6231c2796825b2d095c38bf5b41
Joonho Shin <cck9911@gmail.com>
2022-03-29 03:05:16 +0900

    hw4

---
+++ HW4/web_server.py
@@ -0,0 +1,46 @@
+from socket import *
+
+s = socket()
+s.bind(('', 80))
+s.listen(10)
+
+while True:
+    c, addr = s.accept()
+    data = c.recv(1024)
+    msg = data.decode()
+    req = msg.split('\r\n')
+
+    filename = req[0].split()[1][1:]
+
+    if filename == 'index.html':
+        f = open(filename, 'r', encoding='utf-8')
+        mimeType = 'text/html'
+        data = f.read()
+        c.send(b'HTTP/1.1 200 OK\r\n')
+        c.send(b'Content-Type: ' + mimeType.encode() + b'\r\n')
+        c.send(b'\r\n')
+        c.send(data.encode('euc-kr', 'ignore'))
+    elif filename == 'iot-1.png':
+        f = open(filename, 'rb')
+        mimeType = 'image/png'
+        data = f.read()
+        c.send(b'HTTP/1.1 200 OK\r\n')
+        c.send(b'Content-Type: ' + mimeType.encode() + b'\r\n')
+        c.send(b'\r\n')
+        c.send(data)
+    elif filename == 'favicon-1.ico':
+        f = open(filename, 'rb')
+        mimeType = 'image/x-icon'
+        data = f.read()
+        c.send(b'HTTP/1.1 200 OK\r\n')
+        c.send(b'Content-Type: ' + mimeType.encode() + b'\r\n')
+        c.send(b'\r\n')
+        c.send(data)
+    else:
+        c.send(b'HTTP/1.1 404 Not Found\r\n')
+        c.send(b'\r\n')
+        c.send(b'<HTML><HEAD><TITLE>Not Found</TITLE></HEAD>')
+        c.send(b'<BODY>Not Found</BODY></HTML>')
+
+
+    c.close()
